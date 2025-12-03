fbe35a64144abd8a1933ca12d153c835bf2db88a
Parshva <codewithpasshu@gmail.com>
2025-12-03 18:34:07 +0530

    This and call,apply,bind added

--- 01-12-2025 Practice OOPS/script.js
+++ 01-12-2025 Practice OOPS/script.js
@@ -45,3 +45,116 @@
 // let pen4 = new Createpen("Saino smile", "red");
 
 // Like in upper one the company and classic function we have seen that are reapt always and stores all memory to all new instance for saving memory and make code cleans we have to make prototype to shared memory for reapiting items
+
+// This Ki values
+
+// Globle me window
+// console.log(this);
+
+// Function me window
+// function abcd() {
+//   console.log(this);
+// }
+// abcd()
+
+// object ke under function ki object
+
+// let obj = {
+//   uname: "Parshva",
+//   fnc: function () {
+//     console.log(this);
+//   },
+// };
+
+// obj.fnc();
+
+// es6 function inside obj is window
+// let obj = {
+//   uname: "Parshva",
+//   fnc: () => {
+//     console.log(this);
+//   },
+// };
+
+// obj.fnc()
+
+// es5 function inside es5 function is window in object
+// let obj = {
+//   uname: "Parshva",
+//   fnc: function () {
+//     function defg() {
+//       console.log(this);
+//     }
+//     defg()
+//   },
+// };
+
+// obj.fnc();
+
+// es5 function inside es6 function is object in object
+
+// let obj = {
+//   uname: "Parshva",
+//   fnc: function () {
+//     let defg = () => {
+//       console.log(this);
+//     };
+//     defg();
+//   },
+// };
+
+// obj.fnc();
+
+// es6 function inside es5 function is window in object
+
+// let obj = {
+//   uname: "Parshva",
+//   fnc: () => {
+//     function defg() {
+//       console.log(this);
+//     }
+//     defg();
+//   },
+// };
+
+// obj.fnc();
+
+// Call apply Bind
+
+// call
+// let obj = {
+//     uname:"parshva"
+// }
+
+// function abcd(a,b,c){
+//     console.log(this,a,b,c,);
+
+// }
+
+// abcd.call(obj.uname,1,23,4)
+
+// apply
+// let obj = {
+//   uname: "parshva",
+// };
+
+// function abcd(a, b, c) {
+//   console.log(this, a, b, c);
+// }
+
+// abcd.apply(obj.uname, [1, 23, 4]);
+
+// bind
+
+// let obj = {
+//   uname: "parshva",
+// };
+
+// function abcd(a, b, c) {
+//   console.log(this, a, b, c);
+// }
+
+// let a = abcd.bind(obj.uname, 1, 23, 4);
+// a();
+
+// Bind ek naya function deta hai to hame ek variable me save kar na pad ta hai
